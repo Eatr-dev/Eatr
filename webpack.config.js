6679ec34536e1ce09478878370706483b044040e
ywy-w <yyngwong@gmail.com>
2022-04-12 14:34:41 -0400

    fixing our first cleanup attempt.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
-import webpack from 'webpack';
 import path, {dirname} from 'path';
 import { fileURLToPath } from 'url';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import BundleAnalyzerPlugin from 'webpack-bundle-analyzer';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const config = {
@@ -11,57 +9,21 @@ const config = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  mode: "development",
+  mode: process.env.NODE_ENV,
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: 'babel-loader',
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
         exclude: /node_modules/,
       },
       {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-            },
-          },
-          'postcss-loader',
-        ],
-        exclude: /\.module\.css$/,
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-              modules: true,
-            },
-          },
-          'postcss-loader',
-        ],
-        include: /\.module\.css$/,
-      },
-      {
-        test: /\.svg$/,
-        use: 'file-loader',
-      },
-      {
-        test: /\.png$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              mimetype: 'image/png',
-            },
-          },
-        ],
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
@@ -72,10 +34,6 @@ const config = {
     proxy: {'/': 'http://localhost:3000/'}
   },
   plugins: [
-    // new BundleAnalyzerPlugin.BundleAnalyzerPlugin({
-    //   analyzerMode: 'static',
-    //   openAnalyzer: false,
-    // }),
     new HtmlWebpackPlugin({
       title: 'Development',
       template: 'client/index.html'
